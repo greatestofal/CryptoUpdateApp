097edc4b8fb397cccac198605bba6a2c3bf20421
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-30 11:10:13 +0100

    Done!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,17 @@ const Footer = () => {
     <div className="bg-gray-800 text-white h-14 items-center flex">
       <div className="wrapper-container  w-full">
         {/* logo */}
-        <div className="flex items-center gap-1 cursor-pointer text-center items-center">
+        <div className="flex justify-center gap-3 items-center text-center items-center">
           Created by:
-          <p className="font-semibold ">Greatest of All</p>
+          <p className="font-semibold ">
+            <a
+              href="https://github.com/greatestofal"
+              className="text-red-500 cursor-pointer hover:underline"
+              target="_blank"
+            >
+              Greatest of All
+            </a>
+          </p>
         </div>
       </div>
     </div>
